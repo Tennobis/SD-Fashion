12f9cb68d366a5111c801b304d77f533d720c5a7
Tennobis <tanveer2004hossain@gmail.com>
2025-03-18 20:15:14 +0530

    fix: remove exposedHeaders from CORS configuration

--- backend/src/index.js
+++ backend/src/index.js
@@ -23,7 +23,6 @@ app.use(cors({
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
-  exposedHeaders: ['Set-Cookie']
 }));
 app.use(cookieParser());
 app.use(express.json());
